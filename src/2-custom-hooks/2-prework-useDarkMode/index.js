7d9df65ee8f1176a0e590d8564ed5c00371fd08d
luis Suarez <luisma.suarez@hotmail.com>
2019-08-14 04:01:08 -0500

    styles for hooks 2

--- src/2-custom-hooks/2-prework-useDarkMode/index.js
+++ src/2-custom-hooks/2-prework-useDarkMode/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function UseDarkMode() {
   return (
-    <div className="fl w-90 ph3 pv3">
+    <div className="fl w-90 ph3 pv3 hooks2-darkMode">
       <input
         type="checkbox"
         id="scales"
